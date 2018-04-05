751733f2e9ac58f43f6e69e96bb09563108fbacb
Chase Lau <chase9@mac.com>
2018-04-05 11:19:25 -0500

    Added labels for address and AQ average

--- js/index.js
+++ js/index.js
@@ -1,23 +1,45 @@
-angular.module('myApp', ['ngMap']).controller('MyCtrl', function(NgMap, $http) {
+angular.module('myApp', ['ngMap']).controller('MyCtrl', function (NgMap, $http) {
     var vm = this;
 
     vm.center = 'Minneapolis';
-    vm.aqTable = [];
+    vm.address = 'Minneapolis';
+    vm.aqAverage = 0.0;
 
-    NgMap.getMap().then(function(map) {
+    NgMap.getMap().then(function (map) {
         vm.map = map;
     });
 
-    vm.updateLocation = function() {
-        var center = vm.map.getCenter();
+    vm.updateLocation = function () {
+        let center = vm.map.getCenter();
         vm.center = center.lat() + "," + center.lng();
 
-        console.log(vm.center);
+        $.ajax("https://nominatim.openstreetmap.org/reverse?format=json&lat=" + center.lat() + "&lon=" + center.lng())
+            .then((response) => {
+                vm.address = response['display_name'];
+            });
 
         $http.get("https://api.openaq.org/v1/measurements?coordinates=" + vm.center)
-            .then(function(response){
-                vm.aqTable = response.data;
-                console.log(vm.aqTable);
+            .then(function (response) {
+                vm.aqAverage = averageAQ(response.data);
             });
     };
 });
+
+
+/**
+ * This function finds the average of air quality measurements returned from openAQ.
+ * @param aqResponse the response data from a call to api.openaq.org/v1/measurements
+ * @returns {number} a double indicating the air quality average of the measurements
+ */
+function averageAQ(aqResponse) {
+    let aqAverage = 0.0;
+    let count = 0;
+
+    // Iterate through each of the aq results
+    $.each(aqResponse['results'], (i, item) => {
+        aqAverage += item.value;
+        count = i + 1;
+    });
+
+    return aqAverage / count;
+}
